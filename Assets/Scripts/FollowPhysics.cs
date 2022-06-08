9ec564c1729fc17699af29b3a28d50b555906d32
Takumi Aoyama <aotaku4062@gmail.com>
2022-06-08 18:23:52 +0900

    ・物をつかめるように設定 ・コントローラーの表示を変更

---
+++ Assets/Scripts/FollowPhysics.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPhysics : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void FixedUbdate(){
+        rb.MovePosition(target.transform.position);
+    }
+}
